0c08e6d1052055f831c68607f63167f5e208c822
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-05-27 19:52:07 +0530

    Ready for deploy

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  }, 
   withCredentials: true,
 });

6f56c910cd2c868edc9c42f4316a8d0ea8dcf55c
Mohit Kumar <136831716+MOHIT-IITP@users.noreply.github.com>
2025-06-27 11:53:58 +0530

    updated

--- backend/src/lib/jwtVerify.js
+++ backend/src/lib/jwtVerify.js
@@ -4,10 +4,10 @@ export const genAuthToken = async (UserId, res) => {
     expiresIn: "7d",
   });
   res.cookie("token", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, 
+    httpOnly: true, 
+    sameSite: "none",
+    secure: true,
   });
   return token;
 };
